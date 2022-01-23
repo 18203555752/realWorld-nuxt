ef51a424cc2ef72b0ec9f4d909dea6417c70f3ea
18203555752 <519688153@qq.com>
2022-01-23 11:09:44 +0800

    init

---
+++ study_demo/pages-asyncData/about.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+      <h1>aboutPage页面</h1>      
+      <router-link to="/">首页</router-link>
+  </div>
+</template>
+
+<script>
+export default {
+    name:'aboutPage',
+    layout:'foo',
+    methods:{
+        onClick(){
+            this.$router.push('/')
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
